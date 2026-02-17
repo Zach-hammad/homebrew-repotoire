76bd4f46284dbfb41c8f47ca195ef9f2a48650b3
Zach Hammad <zachariahammad@gmail.com>
2026-02-17 10:17:04 -0500

    Update to v0.3.105 (114 detectors, zero-dep binary)

--- Formula/repotoire.rb
+++ Formula/repotoire.rb
@@ -1,7 +1,7 @@
 class Repotoire < Formula
-  desc "Graph-powered code analysis with 112 detectors"
+  desc "Graph-powered code analysis with 114 detectors"
   homepage "https://github.com/Zach-hammad/repotoire"
-  version "0.3.100"
+  version "0.3.105"
   license "MIT"
 
   on_macos do
